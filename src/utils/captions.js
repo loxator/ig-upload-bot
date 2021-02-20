33f6aef946ecd07ff1eb2b5380be65a67b1ff9f5
Amin Limbada <amin.limbada@consensys.net>
2021-02-20 23:03:54 +0400

    Update hashtags

--- src/utils/captions.js
+++ src/utils/captions.js
@@ -1,6 +1,6 @@
-const imageTags = `#satisfaction #satisfying #love #satisfyingvideos  #oddlysatisfying #satisfy  #happiness #satisfyingsounds  #life #happy  #satisfyingvideo #art  #relax  #reddit #asmrcommunity #instagood #follow  #clean #bhfyp`;
+const imageTags = `#satisfaction #satisfying #satisfyingvideos #oddlysatisfying #satisfy  #satisfyingvideo #relax #reddit`;
 
-const videoTags = `#satisfaction #satisfying #asmr #love #satisfyingvideos  #oddlysatisfying #satisfy #motivation #happiness #satisfyingsounds  #life #happy #asmrsounds #satisfyingvideo #art #asmrvideo #relax #satisfyingsound #reddit #asmrcommunity #instagood #follow  #clean #bhfyp`;
+const videoTags = `#satisfaction #satisfying #satisfyingvideos #oddlysatisfying #satisfy`;
 
 module.exports = {
   imageTags,
